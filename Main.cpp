80c13d4051099254650aa7f5b4bcc5b63ad17d8a
Arimfexendrapus <76406978+Arimfexendrapus@users.noreply.github.com>
2021-09-27 09:07:29 -0400

    Create Main.cpp
    so i guess add stuff into here

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+/*
+
+Name : Tyler Felts, Ethan Cox, Drew Faust, Connor Bass
+Date : 09-27/2021
+Class : CSC 1720
+
+Description : Algorithms Project
+
+Target grade : A
+
+*/
+
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+
+
+
+using namespace std;
