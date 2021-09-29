6bc1d692a6bd5d507e68932591734a74e83ba95f
Arimfexendrapus <tylerfelts@live.com>
2021-09-29 09:54:34 -0400

    added timing code for 4 sorts

--- ALGO_Project1/Main.cpp
+++ ALGO_Project1/Main.cpp
@@ -53,6 +53,121 @@ using namespace std;
 
 int main()
 {
-    
-    
+    //testing bubble sort
+
+    int array[1000];
+    randomArray(array, 1000, 1000);
+    double times[1000];
+
+    for (int i = 0; i < 1000; i++)
+    {
+        using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
+        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+
+        bubbleSort(array, 1000);
+
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+
+        cout << fixed;   //Prevents number being represented in scientific notation
+        times[i] = chrono::duration_cast<fpseconds>(end - start).count();
+
+        randomArray(array, 1000, 1000);
+    }
+
+    double sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        sum = times[i] + sum;
+    }
+    double average = sum / 1000;
+    cout << "the average time to sort an array of 1000 elements using bubble sort is: " << average <<" over 1000 intervals" <<endl;
+
+
+    //testing exchange sort
+
+
+    randomArray(array, 1000, 1000);
+    double times2[1000];
+
+    for (int i = 0; i < 1000; i++)
+    {
+        using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
+        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+
+        exchangeSort(array, 1000);
+
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+
+        cout << fixed;   //Prevents number being represented in scientific notation
+        times2[i] = chrono::duration_cast<fpseconds>(end - start).count();
+
+        randomArray(array, 1000, 1000);
+    }
+
+    sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        sum = times2[i] + sum;
+    }
+    average = sum / 1000;
+    cout << "the average time to sort an array of 1000 elements using exchange sort is: " << average << " over 1000 intervals" << endl;
+
+
+    //testing heap sort
+
+
+    randomArray(array, 1000, 1000);
+    double times3[1000];
+
+    for (int i = 0; i < 1000; i++)
+    {
+        using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
+        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+
+        heapSort(array, 1000);
+
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+
+        cout << fixed;   //Prevents number being represented in scientific notation
+        times3[i] = chrono::duration_cast<fpseconds>(end - start).count();
+
+        randomArray(array, 1000, 1000);
+    }
+
+    sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        sum = times3[i] + sum;
+    }
+    average = sum / 1000;
+    cout << "the average time to sort an array of 1000 elements using heap sort is: " << average << " over 1000 intervals" << endl;
+
+    //testing insertion sort
+
+    randomArray(array, 1000, 1000);
+    double times4[1000];
+
+    for (int i = 0; i < 1000; i++)
+    {
+        using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
+        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+
+        insertionSort(array, 1000);
+
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+
+        cout << fixed;   //Prevents number being represented in scientific notation
+        times4[i] = chrono::duration_cast<fpseconds>(end - start).count();
+
+        randomArray(array, 1000, 1000);
+    }
+
+    sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        sum = times4[i] + sum;
+    }
+    average = sum / 1000;
+    cout << "the average time to sort an array of 1000 elements using insertion sort is: " << average << " over 1000 intervals" << endl;
+
 }
