ab0cdb21dadc38557f030f2aa6be21ba5f9f5165
EthanC2 <ethanrcox@gmail.com>
2021-10-01 09:47:00 -0400

    Reworked Main.cpp to work with an array of any length

--- ALGO_Project1/Main.cpp
+++ ALGO_Project1/Main.cpp
@@ -17,6 +17,7 @@ Target grade : A
 
 #include <iostream>
 #include <chrono>
+#include <algorithm>     //the cardinal sin
 
 #include "Bubble_Sort.cpp"
 #include "Exchange_Sort.cpp"
@@ -35,19 +36,18 @@ Target grade : A
 using namespace std;
 using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
 
-#define SIZE(array) sizeof(array) / sizeof(array[0])
-
 int main()
 {
     //testing bubble sort
     Dataset<int,1000,RANDOM> array;
-    double times[1000];
 
-    for (int i = 0; i < 1000; i++)
+    double times[array.length];
+
+    for (int i = 0; i < array.length; i++)
     {
         chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
 
-        bubbleSort<int>(array, 1000);
+        bubbleSort<int>(array, array.length);
 
         chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
 
@@ -58,25 +58,25 @@ int main()
     }
 
     double sum = 0;
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         sum = times[i] + sum;
     }
-    double average = sum / 1000;
-    cout << "the average time to sort an array of 1000 elements using bubble sort is: " << average <<" over 1000 intervals" <<endl;
+    double average = sum / array.length;
+    cout << "the average time to sort an array of 1000 elements using bubble sort is: " << average <<" over " << array.length << " intervals" <<endl;
 
 
     //testing exchange sort
 
 
     array.genNewData();
-    double times2[1000];
+    double times2[array.length];
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
 
-        exchangeSort<int>(array, 1000);
+        exchangeSort<int>(array, array.length);
 
         chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
 
@@ -86,26 +86,21 @@ int main()
         array.genNewData();
     }
 
-    sum = 0;
-    for (int i = 0; i < 1000; i++)
-    {
-        sum = times2[i] + sum;
-    }
-    average = sum / 1000;
-    cout << "the average time to sort an array of 1000 elements using exchange sort is: " << average << " over 1000 intervals" << endl;
+    average = sum / array.length;
+    cout << "the average time to sort an array of 1000 elements using exchange sort is: " << average << " over " << array.length << " intervals" << endl;
 
 
     //testing heap sort
 
 
     array.genNewData();
-    double times3[1000];
+    double times3[array.length];
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
 
-        heapSort<int>(array, 1000);
+        heapSort<int>(array, array.length);
 
         chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
 
@@ -116,23 +111,23 @@ int main()
     }
 
     sum = 0;
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         sum = times3[i] + sum;
     }
-    average = sum / 1000;
-    cout << "the average time to sort an array of 1000 elements using heap sort is: " << average << " over 1000 intervals" << endl;
+    average = sum / array.length;
+    cout << "the average time to sort an array of 1000 elements using heap sort is: " << average << " over " << array.length << " intervals" << endl;
 
     //testing insertion sort
 
     array.genNewData();
-    double times4[1000];
+    double times4[array.length];
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
 
-        insertionSort<int>(array, 1000);
+        insertionSort<int>(array, array.length);
 
         chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
 
@@ -143,11 +138,11 @@ int main()
     }
 
     sum = 0;
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < array.length; i++)
     {
         sum = times4[i] + sum;
     }
-    average = sum / 1000;
-    cout << "the average time to sort an array of 1000 elements using insertion sort is: " << average << " over 1000 intervals" << endl;
+    average = sum / array.length;
+    cout << "the average time to sort an array of 1000 elements using insertion sort is: " << average << " over " << array.length << " intervals" << endl;
 
 }
