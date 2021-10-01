d3dc834fe18e64ae4469d75ff4df5e1183bb4c2d
EthanC2 <ethanrcox@gmail.com>
2021-10-01 16:41:36 -0400

    forgot what I did lmao

--- ALGO_Project1/Main.cpp
+++ ALGO_Project1/Main.cpp
@@ -14,16 +14,16 @@ Target grade : A
 #include <chrono>
 #include <algorithm>     //the cardinal sin
 
-#include "Bubble_Sort.cpp"
-#include "Exchange_Sort.cpp"
-#include "Insertion_Sort.cpp"
-#include "Merge_Sort.cpp"
-//#include "Quick_Sort.cpp"
-#include "Selection_Sort.cpp"
+#include "BubbleSort.cpp"
+#include "ExchangeSort.cpp"
+#include "InsertionSort.cpp"
+#include "MergeSort.cpp"
+#include "QuickSort.cpp"
+#include "SelectionSort.cpp"
 #include "Swap.cpp"
 #include "Dataset.hpp"
-#include "Heap_Sort.cpp"
-#include "Reverse_Exchange_Sort.cpp"
+#include "HeapSort.cpp"
+#include "ReverseExchangeSort.cpp"
 
 using namespace std;
 using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seconds
@@ -35,9 +35,9 @@ using fpseconds = chrono::duration<double, ratio<1, 1>>;   //Floating-point seco
 #define SIZE(array) sizeof(array) / sizeof(array[0])
 
 //Simple average function
-double average(double array[], const size_t size)
+long double average(double array[], const size_t size)
 {
-    double sum = 0;
+    long double sum = 0;
 
     for(size_t i=0; i < size; i++)
     {
@@ -45,7 +45,6 @@ double average(double array[], const size_t size)
     }
     
     return sum / size;
-    //return std::accumulate(&array[0], &array[0] + size, 0) ;// / size;
 }
 
 // DRIVER CODE //
@@ -56,24 +55,28 @@ int main()
     const int times = 1000;         //The amount of times to test each algorithm
     double times1[times]; 
 
+    //Timepoints for measuring times (elapsed time = end - start)
+    chrono::high_resolution_clock::time_point start;
+    chrono::high_resolution_clock::time_point end;
+
     //Prevents number being represented in scientific notation
     cout << fixed; 
 
     // BUBBLE SORT //
     for (int i = 0; i < times; i++)
     {
-        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+        start = chrono::high_resolution_clock::now();  //Start time
 
         bubbleSort<int>(array, array.length);
 
-        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+        end = chrono::high_resolution_clock::now();  //End time
 
         times1[i] = chrono::duration_cast<fpseconds>(end - start).count();
 
         array.genNewData();
     }
 
-    cout << "The average time to sort an array of " << array.length << " elements using bubble sort is: " << average(times1, times) << " over " << times << " intervals" <<endl;
+    cout << "The average time to sort an array of " << array.length << " elements using bubble sort is: " << average(times1, times) << "s over " << times << " intervals" << endl;
 
     // EXCHANGE SORT //
     array.genNewData();
@@ -81,18 +84,18 @@ int main()
 
     for (int i = 0; i < times; i++)
     {
-        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+        start = chrono::high_resolution_clock::now();  //Start time
 
         exchangeSort<int>(array, array.length);
 
-        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+        end = chrono::high_resolution_clock::now();  //End time
 
         times2[i] = chrono::duration_cast<fpseconds>(end - start).count();
 
         array.genNewData();
     }
 
-    cout << "The average time to sort an array of " << array.length << " elements using exchange sort is: " << average(times2, times) << " over " << times << " intervals" << endl;
+    cout << "The average time to sort an array of " << array.length << " elements using exchange sort is: " << average(times2, times) << "s over " << times << " intervals" << endl;
 
 
     // HEAP SORT //
@@ -101,18 +104,18 @@ int main()
 
     for (int i = 0; i < times; i++)
     {
-        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+        start = chrono::high_resolution_clock::now();  //Start time
 
         heapSort<int>(array, array.length);
 
-        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+        end = chrono::high_resolution_clock::now();  //End time
 
         times3[i] = chrono::duration_cast<fpseconds>(end - start).count();
 
         array.genNewData();
     }
 
-    cout << "The average time to sort an array of " << array.length  << " elements using heap sort is: " << average(times3, times) << " over " << times << " intervals" << endl;
+    cout << "The average time to sort an array of " << array.length  << " elements using heap sort is: " << average(times3, times) << "s over " << times << " intervals" << endl;
 
 
 
@@ -122,17 +125,17 @@ int main()
 
     for (int i = 0; i < array.length; i++)
     {
-        chrono::steady_clock::time_point start = chrono::steady_clock::now();  //Start time
+        start = chrono::high_resolution_clock::now();  //Start time
 
         insertionSort<int>(array, array.length);
 
-        chrono::steady_clock::time_point end = chrono::steady_clock::now();  //End time
+        end = chrono::high_resolution_clock::now();  //End time
 
         times4[i] = chrono::duration_cast<fpseconds>(end - start).count();
 
         array.genNewData();
     }
 
-    cout << "The average time to sort an array of " << array.length << " elements using insertion sort is: " << average(times4, times) << " over " << times << " intervals" << endl;
+    cout << "The average time to sort an array of " << array.length << " elements using insertion sort is: " << average(times4, times) << "s over " << times << " intervals" << endl;
 
-}
+}                                                               
