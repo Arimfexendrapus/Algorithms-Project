28b1cfab99a213e3cc542903ac3b53e7d5e9665b
Connor Bass <cbass2@highpoint.edu>
2021-10-06 13:48:50 -0400

    Augmented Exchange

--- ALGO_Project1/ExchangeSort.cpp
+++ ALGO_Project1/ExchangeSort.cpp
@@ -11,15 +11,20 @@ double exchangeSort(T array[], const size_t size, double& swaps)
 
     //compare count
     double compareCount = 0;
-
+    //sorted bool
+    bool notSorted = true;
     //For every element in the array
-    for (size_t i = 0; i < size-1; i++)
+    for (size_t i = 0; i < size-1 && notSorted; i++)
     {
+        //set flag false
+        notSorted = false;
         //Compare every element after it to the current element (i), swapping if needed
         for (size_t j = i + 1; j < size; j++)
         {
             if (array[i] > array[j])
             {
+                //mark as not sorted if not sorted
+                notSorted = true;
                 Swap<T>(array[i], array[j]);
                 swaps++;
             }
